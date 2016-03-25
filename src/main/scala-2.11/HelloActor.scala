fc17926d5356d5fee3aad55f107ec31b561c4907
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-25 14:08:17 +0000

    Initial commit

---
+++ src/main/scala-2.11/HelloActor.scala
@@ -0,0 +1,9 @@
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+/**
+  * Created by Workstation on 25/03/16.
+  */
+class HelloActor extends Actor {
+  override def receive: Receive = ???
+}
